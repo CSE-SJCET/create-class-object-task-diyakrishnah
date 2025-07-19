12825e2fde6a9be1376fb882ffc23861b88851a2
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2025-07-20 00:27:23 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ class Student{
           System.out.println("Name:\t"+name);
           System.out.println("rollnumber:\t"+rollNumber);
           System.out.println("grade:\t"+grade);
+     }
+}
 public class Main {
       public static void main(String[] args) {
         Student student1=new Student();
