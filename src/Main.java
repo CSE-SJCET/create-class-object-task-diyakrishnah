7c75888c9593e910b0af00ed08d0e452f8764989
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2025-07-20 00:23:05 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,10 @@ class Student{
           System.out.println("grade:\t"+grade);
 public class Main {
       public static void main(String[] args) {
-        Student.student=new Student();
-           student.displayDetails();
+        Student.student1=new Student();
+           student1.name ="Diya";
+           student1.rollNumber =33;
+           student1.grade ='A';
+           student1.displayDetails();
     }
 }
